c40ec17751ecd72f8a6d1dc95a94940ae0f0d24a
taiphat99 <taiphatlqp@gmail.com>
2023-08-17 16:03:08 +0700

    ss1

---
+++ ss1/demo.js
@@ -0,0 +1,41 @@
+// const arr = [1,2,3,4,5];
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+// let names = ['John', 'Mac', 'Peter'];
+// let lengths = names.map(name => name.length);
+
+// console.log(lengths);
+
+
+// const array = [1, 2, 3, 4, 5];
+// array.forEach((element,index) => {
+//     if(index%2==0){
+//     console.log(element);
+//     }
+// });
+
+// let a = (x,y) => x+y;
+// console.log(a(5,6));
+
+
+
+// const arr = [1,2,3,4,5];
+// const newArr = arr.map((element,index) => {
+//     return index;
+// });
+// console.log(newArr);
+// const array = arr.map((element,index,arr) => {
+//     return arr;
+// });
+// console.log(array);
+
+
+// let a = (name) => "Hi " + name;
+// console.log(a("Danny"));
+
+let arr = ["danny","chunky"];
+let newArr = ["jenny","franky"];
+let finalArr= [...arr,...newArr];
+console.log(finalArr);
---
+++ ss1/es6.js
@@ -0,0 +1,76 @@
+let courses = [ 
+    { 
+      id: 1, 
+      title: "ReactJS Tutorial", 
+      rating: 4.2, 
+    }, 
+    { 
+      id: 2, 
+      title: "Angular Tutorial", 
+      rating: 2.5, 
+    }, 
+    { 
+      id: 3, 
+      title: "VueJS Tutorial", 
+      rating: 3.8, 
+    }, 
+    { 
+      id: 4, 
+      title: "Java Tutorial", 
+      rating: 4, 
+    }, 
+    { 
+      id: 5, 
+      title: "JavaScript Tutorial", 
+      rating: 3.5, 
+    }, 
+  ];
+
+
+  let addedCourses = [ 
+    { 
+      id: 6, 
+      title: "PHP Tutorial", 
+      rating: 3, 
+    }, 
+    { 
+      id: 7, 
+      title: "C# Tutorial", 
+      rating: 2, 
+    }, 
+    { 
+      id: 8, 
+      title: "Docker Tutorial", 
+      rating: 3.8, 
+    } 
+  ]; 
+
+
+
+    // First Request
+    let firstRequest = courses.filter(checkRate);
+    function checkRate({rating}){
+        return rating >=4;
+    }
+    console.log(firstRequest);
+
+
+
+    // Second Request
+    let secondRequest = courses.filter(({rating}) => rating <4).map(toString);
+    function toString({id,title,rating}){
+            return id + " - " + title + " - " + rating;
+    }
+    console.log(secondRequest);
+
+
+
+    // Third Request
+
+
+    let finalCourses = [...courses,...addedCourses];
+    console.log(finalCourses);
+
+
+
+
