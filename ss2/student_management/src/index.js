151059b3257c8e565ecb8dca6cdb5abf2ba1c72d
taiphat99 <taiphatlqp@gmail.com>
2023-08-18 14:04:52 +0700

    ss2

---
+++ ss2/student_management/src/index.js
@@ -0,0 +1,59 @@
+import ReactDOM from "react-dom/client";
+import './index.css';
+
+
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+<div>
+  <h1>Student Management</h1>
+ <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  {students.map((student,index) => (
+    <tr key={index}>
+      <td>{student.company}</td>
+      <td>{student.contact}</td>
+      <td>{student.country}</td>
+    </tr>
+  )
+  )}
+ </table>
+ </div>
+);
